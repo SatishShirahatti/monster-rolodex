5a0b61c8d87492ad6dde8e09a92e5baf24aaaadc
satishshirahatti <shirahatti.satish2010@gmail.com>
2020-05-02 10:26:17 +0200

    Updates

---
+++ precache-manifest.ded8a29ef4f5f3fc24e9fb4179d0e9a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205626f732c037d5de734a1cbfcd6ac7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "09836b02da2ba5e348ba",
+    "url": "/monster-rolodex/static/css/main.685b6e82.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09836b02da2ba5e348ba",
+    "url": "/monster-rolodex/static/js/main.bb64fa87.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
